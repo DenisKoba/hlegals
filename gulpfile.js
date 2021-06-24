cdb5ba83818f6a4acb66d11c50ae9d46bcbe429e
denis.koba <denis.koba@socialtechnologies.io>
2021-06-24 20:18:18 +0300

    fixes gulp and file structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,45 @@ const gulp = require('gulp'),
       sass = require('gulp-sass'),
       fileinclude = require('gulp-file-include'),
       browserSync = require('browser-sync');
+      replace = require('gulp-replace');
+
+gulp.task('copy:images', function() {
+    return gulp.src('./src/assets/icons/*')
+        .pipe(gulp.dest('./dist/assets/icons'))
+});
 
 gulp.task('sass', function(){
-    return gulp.src("./scss/**/*.scss")
+    return gulp.src("./src/assets/scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./dist/css', [1]))
         .pipe(browserSync.stream());
 });
 
 gulp.task('fileinclude', function() {
     return gulp.src([
+        './src/pages/contacts/contacts.html',
         './src/pages/about_us/about_us.html',
         './src/pages/homepage/index.html'
     ]).pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task("watch", function(done) {
     browserSync.init({
-        server:'.'
+        server: {
+            baseDir: "./dist",
+            // replace current page
+            index: "index.html"
+        }
     })
 
-    gulp.watch("./scss/**/*.scss", gulp.series('sass'));
-    gulp.watch("./pages/*.html").on('change', gulp.series('fileinclude'));
+    gulp.watch("./src/**/*.scss", gulp.series('sass'));
+    gulp.watch("./src/pages/*.html").on('change', gulp.series('fileinclude'));
     gulp.watch('./**/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series('watch'));
+gulp.task('default', gulp.series(['sass', 'fileinclude', 'watch', 'copy:images']));
